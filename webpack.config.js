23b6e18f99a03bf975e9fc45668090dd3f219115
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-01-24 14:01:07 +0200

    Configuration for  HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+// Entry accepts a path or an object of entries.
+// We'll be using the latter form given it's
+// convenient with more complex configurations.
+//
+// Entries have to resolve to files! It relies on Node.js
+// convention by default so if a directory contains *index.js*,
+// it will resolve to that.
+
+const common = {
   entry: {
     app: PATHS.app,
   },
@@ -16,7 +25,55 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack demo'
+      title: 'Webpack demo',
     }),
   ],
+};
+
+const developmentConfig = {
+  devServer: {
+    // Enable history API fallback so HTML5 History API based
+    // routing works. This is a good default that will come
+    // in handy in more complicated setups.
+    historyApiFallback: thue,
+
+    // Unlike the cli flag, this doesn't set
+    // HotModuleReplacementPlugin!
+    hot: true,
+
+    // Don't refresh if hot loading fails. If you want
+    // refresh behavior, set inline: true instead.
+    hotOnly: true,
+
+    // Display only errors to reduce the amount of output.
+    stats: "error-only",
+
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Vagrant or Cloud9, set
+    // host: options.host || '0.0.0.0';
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+  },
+  plugins: [
+    // Enable multi-pass compilation for enhanced performance
+    // in larger projects. Good default.
+    new webpack.HotModuleReplacementPlugin({
+      // Disabled as this won't work with html-webpack-template yet
+      //multiStep: true,
+    }),
+  ],
+};
+
+module.exports = function (env){
+  if (env === 'production') {
+    return common;
+  }
+  return Object.assign({}, common, developmentConfig, {
+      plugins: common.plugins.concat(developmentConfig.plugins),
+    }
+  );
 };
